31d60f73850ee17704a430aa126437567678d64f
Angus McAloon <AMcAloon@lcs.on.ca>
2020-10-26 11:44:39 -0400

    Added different variables in sifty sums to make different sifty sums

--- ShiftySums/main.swift
+++ ShiftySums/main.swift
@@ -15,6 +15,7 @@ print("===========")
 
 // INPUT
 
+print("input shifty num: ")
 // Get the number to be shifted
 let n = Int(readLine()!)!
 
@@ -24,10 +25,16 @@ let k = 3
 // PROCESS
 
 // Shift the given number "k" number of times
-var output = n
-output += n * 10
-output += n * 10 * 10
-output += n * 10 * 10 * 10
+
+var output1 = n
+var output2 = n
+var output3 = n
+
+output1 += n * 10
+output2 += n * 10 * 10
+output3 += n * 10 * 10 * 10
+
+var output =  + output1 + output2 + output3
 
 // OUTPUT
 print(output)
